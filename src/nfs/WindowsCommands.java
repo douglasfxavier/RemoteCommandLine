2036b1a4e8fb72fe0464e2599a2606a535b28723
douglasfxavier <douglasf.xavier@gmail.com>
2019-04-14 13:57:27 -0300

    Add class WindowsCommands

---
+++ src/nfs/WindowsCommands.java
@@ -0,0 +1,44 @@
+package nfs;
+
+import java.io.File;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class WindowsCommands implements CommandFactory{
+
+    ProcessBuilder builder;
+
+    public WindowsCommands() {
+        this.builder = new ProcessBuilder();
+    }
+
+    @Override
+    public ProcessBuilder createDirectory(String folderName) {
+        builder.command("cmd.exe","/c","mkdir",folderName);
+        return builder;
+    }
+
+    @Override
+    public ProcessBuilder createFile(String texto, String fileName) {
+        builder.command("cmd.exe","/c","echo", texto, ">",fileName);
+        return builder;
+    }
+
+    @Override
+    public ProcessBuilder readDirectory(String directory) {
+        builder.command("cmd.exe", "/c","dir " + directory);
+        return builder;
+    }
+
+    @Override
+    public ProcessBuilder renameFile(String currentName, String newName) {
+        builder.command("cmd.exe", "/c","ren ", currentName, newName);
+        return builder;
+    }
+
+    @Override
+    public ProcessBuilder removeFile(String fileName) {
+        builder.command("cmd.exe", "/c","del ", fileName);
+        return builder;
+   }
+}
