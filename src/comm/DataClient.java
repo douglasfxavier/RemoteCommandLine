3ac26d31f33152d97d862295c488c9712141fd85
douglasfxavier <douglasf.xavier@gmail.com>
2019-04-14 13:58:43 -0300

    Add class DataClient

---
+++ src/comm/DataClient.java
@@ -0,0 +1,35 @@
+package comm;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class DataClient {
+
+    public static void main(String[] args) {
+        try {
+            Socket serverSocket = new Socket("localhost",6000);
+
+            PrintWriter serverOutput = new PrintWriter(serverSocket.getOutputStream());
+            Scanner serverInput = new Scanner(serverSocket.getInputStream());
+            Scanner keyBoard = new Scanner(System.in);
+            System.out.println("Conectado ao servidor");
+            while(true) {
+                System.out.print("> ");
+
+                String command = keyBoard.nextLine();
+                serverOutput.println(command);
+                serverOutput.flush();
+
+                while (serverInput.hasNext()){
+                    System.out.println(serverInput.nextLine());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
