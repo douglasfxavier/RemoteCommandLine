bddc642ca024a013c27b4406c7bb6c727ab57861
douglasfxavier <douglasf.xavier@gmail.com>
2019-04-14 13:55:51 -0300

    Add class CommandProcessBuilder

---
+++ src/nfs/CommandProcessBuilder.java
@@ -0,0 +1,25 @@
+package nfs;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.util.concurrent.Executors;
+
+public class CommandProcessBuilder {
+
+    public void exec(ProcessBuilder builder, PrintWriter out) throws IOException, InterruptedException {
+
+        builder.directory(new File(System.getProperty("user.home")));
+        Process process = builder.start();
+
+        StreamGobbler streamGobbler =
+                new StreamGobbler(process.getInputStream(), out::println);
+        Executors.newSingleThreadExecutor().submit(streamGobbler);
+
+        int exitCode = process.waitFor();
+        assert exitCode == 0;
+
+
+    }
+}
